f798e98e9bfc367f8adc172f3a2743aa6982cdc1
zhengxiuyu-at-getstore <zhengxiuyu@getstore.cn>
2019-09-23 18:06:39 +0800

    add gameround term and publish_at

--- src/views/gameround/components/RoundForm.vue
+++ src/views/gameround/components/RoundForm.vue
@@ -21,6 +21,18 @@
           v-model="gameStateDisabled"
         />
       </el-form-item>
+      <el-form-item label="分类">
+        <el-select v-model="selectedTerms" multiple placeholder="请选择">
+          <el-option v-for="term in termList" :key="term.id" :label="term.name" :value="term.id" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="发布时间">
+        <el-date-picker
+          v-model="publish_at"
+          type="datetime"
+          placeholder="选择日期时间"
+        />
+      </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">保存</el-button>
       </el-form-item>
@@ -29,7 +41,7 @@
 </template>
 
 <script>
-import { updateGameRound } from '@/api/backend.js'
+import { updateGameRound, getTermInfo } from '@/api/backend.js'
 export default {
   name: 'RoundForm',
   props: {
@@ -45,7 +57,10 @@ export default {
       formData: {
         name: '',
         time: ''
-      }
+      },
+      termList: [],
+      selectedTerms: [],
+      publish_at: ''
 
     }
   },
@@ -53,11 +68,21 @@ export default {
     // 当gameRound数据改变，重新初始化数据
     'gameRound': 'initData'
   },
-  created() {},
+  created() {
+    getTermInfo().then(async res => {
+      console.log('res----:', res)
+      this.termList = res
+    })
+  },
   mounted() {},
   methods: {
     initData() {
+      console.log('this.gameRound---:', this.gameRound)
       if (this.gameRound != null) {
+        this.publish_at = this.gameRound.publish_at
+        for (let i = 0; i < this.gameRound.termList.length; i++) {
+          this.selectedTerms.push(this.gameRound.termList[i].term_id)
+        }
         if (this.gameRound.state == 'disabled') {
           this.gameStateDisabled = true
         }
@@ -82,13 +107,16 @@ export default {
       } else {
         state = 'disabled'
       }
+      var terms = this.selectedTerms // array of term_id
 
       updateGameRound(this.gameRound.id, {
+        terms: terms,
         gameRound: {
           name: this.formData.name,
           start_at: this.formData.time[0],
           end_at: this.formData.time[1],
-          state: state
+          state: state,
+          publish_at: this.publish_at
         }
       }).then(res => {
         console.log('res====:', res)
