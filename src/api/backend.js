826c8d694ef086c1ea35eaa35d841a56f958dfb1
qinghe <david@getstore.cn>
2019-12-31 10:27:23 +0800

    Revert "fix conflict"
    This reverts commit 15460edc437b8bfa8b778ee9ff8ddf7f7d2ddbd6, reversing
    changes made to 7fe0a2ba9cadad6147bb0bb8eeee6884f5b69185.

--- src/api/backend.js
+++ src/api/backend.js
@@ -56,8 +56,3 @@ export const getResultInfo = (data) => fetch(basePath + '/game_results/getResult
 export const getGameDayInfo = (data) => fetch(basePath + '/game_days/getGameDayInfo', data, 'POST')
 export const getAlbumResultInfo = (data) => fetch(basePath + '/game_results/getAlbumResultInfo', data, 'POST')
 export const getExportInfo = (data) => fetch(basePath + '/export/getExportInfo', data, 'POST')
-
-export const createOtherGameRound = (data) => fetch('/api/othergameround/createOtherGameRound', data, 'POST')
-export const getOtherGameRound = (data) => fetch('/api/othergameround/getOtherGameRound', data, 'POST')
-export const getGameType = (data) => fetch('/api/gametypes/getGameType', data, 'POST')
-export const getGameTypeByType = (data) => fetch('/api/gametypes/getGameTypeByType', data, 'POST')
