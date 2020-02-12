4a9f185e74138c7d1244e0201d6d1c2c020b050e
zhengxiuyu-at-getstore <zhengxiuyu@getstore.cn>
2020-02-12 14:55:27 +0800

    添加大屏端地址

--- src/views/dpgameround/components/GameUrlDialog.vue
+++ src/views/dpgameround/components/GameUrlDialog.vue
@@ -1,6 +1,7 @@
 <template>
   <el-dialog title="活动地址" :visible="computedDialogVisible" @open="showUrlDialog" @close="closeDialog">
     <p>{{ gameUrl }} </p>
+    <a target="_blank" :href="dpgameUrl">{{ dpgameUrl }} </a>
     <img id="share-qrcode-img" style="margin: 0 auto;" :src="gameQrcodeSrc">
   </el-dialog>
 </template>
@@ -25,7 +26,8 @@ export default {
   data() {
     return {
       gameUrl: null,
-      gameQrcodeSrc: null
+      gameQrcodeSrc: null,
+      dpgameUrl: null
     }
   },
   computed: {
@@ -37,6 +39,8 @@ export default {
     showUrlDialog() {
       console.log(' showUrlDialog ', this.gameRound)
       this.gameUrl = baseGameUrl + '/game/' + this.gameRound.code + '/' + this.gameRound.number + '/entry'
+      this.dpgameUrl = baseGameUrl + '/' + this.gameRound.code + '-control.html?number=' + this.gameRound.number
+      // http://127.0.0.1:8080/dpyiy-control.html?number=fb80a02564dfc9ccd9b936fe89953fed
 
       QRCode.toDataURL(this.gameUrl, { type: 'image/png' }, (error, gameurl) => {
         if (error) {
